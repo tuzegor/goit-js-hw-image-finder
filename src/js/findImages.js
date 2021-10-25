42105cc23e0127c5eba2b116e34b384ce8d653c5
Yehor Tuz <tuzegor@gmail.com>
2021-10-26 00:23:14 +0300

    styles

--- src/js/findImages.js
+++ src/js/findImages.js
@@ -1,6 +1,7 @@
 import { fetchImg } from './apiService.js';
 import refs from './refs.js';
 const { formEl, listEl, loadMoreBtn } = refs;
+import template from '../templateCountryCard.hbs';
 // ------------------------------------------
 import { error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
@@ -12,12 +13,31 @@ let currentValue = '';
 
 formEl.addEventListener('submit', event => {
   event.preventDefault();
+  listEl.innerHTML = '';
+  page = 1;
   currentValue = event.target.elements.query.value;
-  fetchImg(currentValue, page).then(result => {
-      console.log(result);
-  });
-
-
-
-  formEl.reset()
+  
+  if (currentValue.length === 0) {
+    error('Enter text');
+  } else {
+    cardsBuilder();
+    formEl.reset();
+  }
+  
+  loadMoreBtn.addEventListener('click', () => cardsBuilder());
 });
+
+
+function createImgCards(result) {
+  listEl.insertAdjacentHTML('beforeend', template(result.hits));
+}
+
+function cardsBuilder() {
+  fetchImg(currentValue, page).then(result => {
+    if (result.total === 0) {
+      error("We don't have such a picture");
+    }
+    createImgCards(result);
+  });
+  page += 1;
+}
