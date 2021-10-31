1e14c24a458554eb96685711137eeafd6fd4a09b
Yehor Tuz <tuzegor@gmail.com>
2021-10-31 23:12:22 +0200

    Добавлен try чтобы отловить ошибку парса

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,9 +7,11 @@ const API_KEY = '24011723-51ecfad92349740cba3768f0d';
 
 
 export function fetchImg(searchQuery, page) {
-  return fetch(BASE_URL + `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`)
-    
+  try{
+    return fetch(BASE_URL + `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`)
     .then(response => response.json())
-    .catch(error => error);
+  } catch {
+    error("Ошибка парса")
+  }
 }
 
