b6a9eb4f50db72845c80b5e4ecffeb1272c97ca7
Yehor Tuz <tuzegor@gmail.com>
2021-11-01 22:41:59 +0200

    Переписал fetch на async await

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,13 +5,12 @@ import '@pnotify/core/dist/BrightTheme.css';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '24011723-51ecfad92349740cba3768f0d';
 
-
-export function fetchImg(searchQuery, page) {
+export async function fetchImg(searchQuery, page) {
   try{
-    return fetch(BASE_URL + `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`)
-    .then(response => response.json())
+    const response = await fetch(BASE_URL + `?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`)
+    const newImg = await response.json()
+    return newImg
   } catch {
     error("Ошибка парса")
   }
 }
-
